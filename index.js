b6aed97bea2abbc058c14be889e3e71905524202
Dmitry Platonov <platonov@trueconf.ru>
2022-12-12 19:20:09 +0300

    add netifly host

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Telegraf } = require("telegraf");
-const { BOT_NAME, BOT_TOKEN } = process.env;
+const { BOT_NAME, BOT_TOKEN, HOST_NAME, PORT } = process.env;
+
 
 const bot = new Telegraf(BOT_TOKEN);
 bot.start((ctx) => ctx.reply("Welcome"));
@@ -13,7 +14,26 @@ bot.on(
         console.log("CHAT :", ctx.update.message.chat);
         // console.log("AGENT :", ctx.telegram.options.agent);
         // console.log("DATE :", new Date(ctx.update.message.date));
+        console.log(bot.secretPathComponent());
     },
 );
+
 bot.hears("hi", (ctx) => ctx.reply("Hey there"));
-bot.launch();
+bot.launch({
+
+    webhook: {
+    // Public domain for webhook; e.g.: example.com
+    domain: HOST_NAME,
+
+    // Port to listen on; e.g.: 8080
+    port: PORT ,
+
+    // Optional path to listen for.
+    // `bot.secretPathComponent()` will be used by default
+    hookPath: "/webhook",
+
+    // Optional secret to be sent back in a header for security.
+    // e.g.: `crypto.randomBytes(64).toString("hex")`
+    // secretToken: randomAlphaNumericString,
+  },
+});
